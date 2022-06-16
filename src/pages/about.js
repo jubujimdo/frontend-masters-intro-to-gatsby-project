4f625562b68858ab072a038ac7c495699cf2493d
Julian Buchholz <julian.buchholz@jimdo.com>
2022-06-16 16:37:04 +0200

    config: add meta data

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage(){
     return (
         <main>
             <h1>About this site</h1>
+            <Link to="/">Back to Home</Link>
         </main>
     );
 }
