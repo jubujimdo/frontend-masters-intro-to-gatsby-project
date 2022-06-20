a2d6c3801577d8a865f8a2dfd406f37892171309
Julian Buchholz <julian.buchholz@jimdo.com>
2022-06-20 16:52:47 +0200

    blog: add blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,27 @@ module.exports = {
         image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
     },
     plugins: [
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+                  resolve: 'gatsby-source-filesystem',
+                 options: {
+                    name: 'posts',
+                    path: `${__dirname}/src/posts/`,
+                },
+              },
+              {
+                  resolve: 'gatsby-plugin-page-creator',
+                  options: {
+                    path: `${__dirname}/src/posts/`,
+                  },
+                },
+                {
+                  resolve: 'gatsby-plugin-mdx',
+                  options: {
+                        defaultLayouts: {
+                            posts: require.resolve('./src/components/post-layout.js')
+                        }
+                  },
+                  },
     ]
 };
