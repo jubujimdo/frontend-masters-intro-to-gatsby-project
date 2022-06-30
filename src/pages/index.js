efaf910dd388c702db4f56ade1dffe774bf8536b
Julian Buchholz <julian.buchholz@jimdo.com>
2022-06-30 10:33:47 +0200

    add last changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,38 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql} from 'gatsby';
 import Layout from '../components/layout';
 
 export default function IndexPage(){
+const data=useStaticQuery(graphql`
+    query GetBlogPosts {
+    allMdx {
+        nodes {
+        id
+        slug
+        frontmatter {
+            title
+            description
+            date(fromNow: true)
+        }
+        }
+    }
+    }
+`);
+
+const posts = data.allMdx.nodes
+
+
 
     return (
         <Layout>
 
             <h1>Hello Frontend Masters!</h1>
             <Link to="/about">To About</Link>
-
+            <h2>Check out my blog posts</h2>
+            <ul>
+                {posts.map((post)=> <li key={post.id}><Link to={post.slug}>{post.frontmatter.title}</Link>{' '}<small>Posted: {post.frontmatter.date}</small></li>)}
+            </ul>
+            {console.log({posts})}
         </Layout>
     );
 }
